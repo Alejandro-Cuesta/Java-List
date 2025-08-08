f9fa6c7c559309b6cafe9e063a674b34652837bb
Alejandro-Cuesta <janittoss@hotmail.com>
2025-08-08 12:14:11 +0200

    feat: añadida estructura para DayListTest.java

---
+++ src/test/DaysListTest.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class DaysListTest {
+    
+    private DaysList daylist;
+
+    @BeforeEach
+    public void setup() {
+        daysList = new DaysList(); // crea nueva instancia antes de cada test
+    } 
+}
