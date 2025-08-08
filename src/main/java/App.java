ddbe02191f86bbd34b3458fd0fb2e1674f25728e
Alejandro-Cuesta <janittoss@hotmail.com>
2025-08-08 14:30:58 +0200

    feat: añadido createDayList en App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Dias de la semana");
+        System.out.println("Días de la semana:");
+
+        DaysList daysList = new DaysList();
+        daysList.createDayList();
+
+        for (String day : daysList.getDays()) {
+            System.out.println(day);
+        }
     }
 }
